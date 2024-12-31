16d3d7955e67077345b37563745d37822dd46471
aitanafernandez23 <aitanafernandezmarzoa2@gmail.com>
2024-12-31 19:46:11 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,56 @@
-document.addEventListener('DOMContentLoaded', function () {
-let imagenAvion = document.getElementById("imagen-avion");
-let imagenGrande = document.getElementById("imagen-grande");
-imagenGrande.onclick = function () {
-let mySrc = imagenGrande.getAttribute('src');
-if (mySrc === 'https://vectorfreefiles.com/media/thumb/fit/501x353/62288acbd3c5b.jpg') {
+let myImage = document.querySelector("#myImage");
+let myButton= document.querySelector ("#myButton");
+let myHeading = document.querySelector ("#user-name");
+let movimiento= document.querySelector ("#movimiento");
 
-imagenGrande.setAttribute('src', 'https://www.shutterstock.com/image-vector/cartoon-
-world-globe-stand-3d-600nw-2280329667.jpg');
-
-} else {
-imagenGrande.setAttribute('src',
-'https://vectorfreefiles.com/media/thumb/fit/501x353/62288acbd3c5b.jpg');
-}
+//Cambiar imagen al clicar
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'https://vectorfreefiles.com/media/thumb/fit/501x353/62288acbd3c5b.jpg') {
+    myImage.setAttribute ('src','https://www.shutterstock.com/image-vector/cartoon-world-globe-stand-3d-600nw-2280329667.jpg');
+  } else {
+    myImage.setAttribute ('src','https://vectorfreefiles.com/media/thumb/fit/501x353/62288acbd3c5b.jpg');
+  }
 };
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
+
+//Pedir al usuario su nombre
 function setUserName() {
-let myName = prompt('Por favor, introduce tu nombre.');
-if (myName && myName !== "") {
-localStorage.setItem('name', myName);
-myHeading.innerHTML = 'Alrededor del mundo, ' + myName;
+  let myName = prompt('Por favor, ingrese su nombre.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Alrededor del mundo,'+ myName;
+  }
 }
-}
-if (!localStorage.getItem('name')) {
-setUserName();
-} else {
 
-let storedName = localStorage.getItem('name');
-myHeading.innerHTML = 'Alrededor del mundo, ' + storedName;
+if (!localStorage.getItem('name')){
+  setUserName();
+}else {
+  let storedName= localStorage.getItem ('name');
+  myHeading.innerHTML= 'Alrededor del mundo'+ storedName;
 }
-myButton.onclick = function () {
-setUserName();
-};
-let posX = 0;
-let maxWidth = window.innerWidth;
-let speed = 2;
-let direction = 1;
-function moverAvion() {
-posX += speed * direction;
-if (posX >= maxWidth - 50 || posX <= 0) {
-direction *= -1;
+
+myButton.onclick = function (){
+  setUserName();
 }
-imagenAvion.style.left = posX + 'px';
-requestAnimationFrame(moverAvion);
+
+
+//animacion del avion
+let posX= 0;
+let posY= 0;
+
+function moverObjeto(){
+  if(posX< window.innerWidth - 50 && posY===0){
+    posX+=5;
+  } else if ( posX >= window.innerWidth - 50 && posY <window.innerHeight - 50) {
+    posY +=5;
+  } else {
+    posX= 0;
+    posY = 0;
+  }
+    movimiento.style.transform = "translate(${posX}px, ${posY}px)";
+   requestAnimationFrame (moverObjeto);
 }
-moverAvion();
-});
+
+moverObjeto();
